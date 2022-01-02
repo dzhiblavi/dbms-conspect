cd92134b30c3f36b78eae2c651136d261f28e85a
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2022-01-02 14:50:23 +0300

    added svgs from all slides

---
+++ assets/kgeorgiy/download.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eux
+
+SLIDES=(
+    "intro"
+    "modelling"
+    "fundeps"
+    "normalization"
+    "relalgebra"
+    "relcalculus"
+    "modification"
+    "indexing"
+    "procedures"
+    "transactions"
+    "optimization"
+    "distributed"
+    "null"
+    "orm"
+    "trees"
+    "temporal"
+)
+
+LINK="https://www.kgeorgiy.info//courses/dbms/slides"
+
+for slides in "${SLIDES[@]}"; do
+    link="$LINK/$slides.xhtml"
+    mkdir "$slides" || true
+    wget -nd -r -P "$slides" -A svg "$link"
+done
